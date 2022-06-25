dddfd28ef3f415251490bbb188fe04b5460eea24
WanjuguNdegwa <99987352+WanjuguNdegwa@users.noreply.github.com>
2022-06-25 23:09:11 +0300

    commit tree 8ff0a1c7d6c8bf06ff862f8dc919d233b2364c08

---
+++ db/migrate/20220625195758_create_reviews.rb
@@ -0,0 +1,10 @@
+class CreateReviews < ActiveRecord::Migration[6.1]
+  def change
+    create_table :reviews do |t|
+      t.integer :score
+      t.string :comment
+      t.integer :game_id
+      t.timestamps
+    end
+  end
+end
